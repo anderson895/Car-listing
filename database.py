fd11344265fed5bb77dbe6b732ace11ba05af760
JARP <105678913+anderson895@users.noreply.github.com>
2025-08-14 11:08:02 +0800

    deploy vercel

--- database.py
+++ database.py
@@ -1,3 +1,18 @@
+import mysql.connector
+
+class Database:
+    def connect(self):
+        conn = mysql.connector.connect(
+            host='auth-db1873.hstgr.io',
+            database='u841289135_data_listing',
+            user='u841289135_data_listing',
+            password='Data_listing2025@',
+            port=3306
+        )
+        return conn
+
+
+
 # import mysql.connector
 
 # class Database:
@@ -9,18 +24,16 @@
 #             password=''
 #         )
 #         return conn
+# import mysql.connector
 
-
-import mysql.connector
-
-try:
-    conn = mysql.connector.connect(
-        host='auth-db1873.hstgr.io',
-        database='u841289135_data_listing',
-        user='u841289135_data_listing',
-        password='Data_listing2025@',
-        port=3306
-    )
-    print("Connected successfully!")
-except mysql.connector.Error as err:
-    print("Error:", err)
+# try:
+#     conn = mysql.connector.connect(
+#         host='auth-db1873.hstgr.io',
+#         database='u841289135_data_listing',
+#         user='u841289135_data_listing',
+#         password='Data_listing2025@',
+#         port=3306
+#     )
+#     print("Connected successfully!")
+# except mysql.connector.Error as err:
+#     print("Error:", err)
