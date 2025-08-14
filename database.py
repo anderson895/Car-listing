2d27ed45e2844549238e51b238cf505f7e1dcdef
JARP <105678913+anderson895@users.noreply.github.com>
2025-08-14 10:52:36 +0800

    update

--- database.py
+++ database.py
@@ -1,11 +1,26 @@
+# import mysql.connector
+
+# class Database:
+#     def connect(self):
+#         conn = mysql.connector.connect(
+#             host='localhost',
+#             database='consultajaime',
+#             user='root',
+#             password=''
+#         )
+#         return conn
+
+
 import mysql.connector
 
-class Database:
-    def connect(self):
-        conn = mysql.connector.connect(
-            host='localhost',
-            database='consultajaime',
-            user='root',
-            password=''
-        )
-        return conn
+try:
+    conn = mysql.connector.connect(
+        host='auth-db1873.hstgr.io',
+        database='u841289135_data_listing',
+        user='u841289135_data_listing',
+        password='Data_listing2025@',
+        port=3306
+    )
+    print("Connected successfully!")
+except mysql.connector.Error as err:
+    print("Error:", err)
